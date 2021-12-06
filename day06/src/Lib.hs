ec979ca9275de5699cc0af6f5d20aae7a746b88e
heinke <git@h9h.de>
2021-12-06 14:14:26 +0100

    Day 06

--- day06/src/Lib.hs
+++ day06/src/Lib.hs
@@ -1,21 +1,80 @@
+{-# LANGUAGE TypeApplications #-}
 module Lib where
 
-type X = Int
+import Data.List.Split ( wordsBy )
+import Data.List ( group, sort, replicate, repeat )
+
+type Idx = Int
 
 puzzle :: IO ()
 puzzle = do
-    input <- lines <$> readFile "src/input.txt"
-    let xxx = parse input
-    print $ take 1 xxx
-    print $ "Part 1: " ++ "todo"
-    print $ "Part 2: " ++ "todo"
+    input <- readFile "src/input.txt"
+    let fishes = parse input
+    print $ "Input: " ++ show fishes
+    let initialPopulation = initialFish fishes
+    print $ "Initial Population: " ++ show initialPopulation
+    print $ "Part 1: " ++ show (sum $ iterate growFish initialPopulation !! 80)
+    print $ "Part 2: " ++ show (sum $ iterate growFish initialPopulation !! 256)
 
 --
 -- Parse Input
 --
+parse :: String -> [Int]
+parse line = map (read @Int) $ wordsBy ( == ',') line
 
-parseLine :: String -> X
-parseLine _ = 1
+initialFish :: [Int] -> [Int]
+initialFish = foldl incr' (replicate 9 0)
 
-parse :: [String] -> [X]
-parse = map parseLine
+--
+-- "Modifying" a list (is a pain)
+--   which means, I'm probably not using the right data structure
+--
+add' :: Int -> Idx -> [Int] -> [Int]
+add' n index = zipWith (+) (addend n index)
+    where
+      addend :: Int -> Idx -> [Int]
+      addend n index
+        | index <= 0 = n : repeat 0
+        | otherwise =  replicate index 0 ++ n : repeat 0
+
+incr' :: [Int] -> Idx -> [Int]
+incr' xs index = add' 1 index xs
+
+rotate :: [a] -> [a]
+rotate = tail <> take 1
+
+--
+-- evolve the fish population
+--
+
+growFish :: [Int] -> [Int]
+growFish xs = let n = head xs in
+    add' n 6 $ rotate xs
+
+{-
+>>>iterate growFish [0,1,1,2,1,0,0,0,0] !! 1
+[1,1,2,1,0,0,0,0,0]
+>>>iterate growFish [0,1,1,2,1,0,0,0,0] !! 2
+[1,2,1,0,0,0,1,0,1]
+
+>>>iterate growFish [0,1,1,2,1,0,0,0,0] !! 3
+[2,1,0,0,0,1,1,1,1]
+-}
+
+--
+-- a better alternative
+--
+growFish' :: [Int] -> [Int]
+growFish' fish = setAtIndex <$> [0..8]
+  where setAtIndex 8 = head fish
+        setAtIndex 6 = head fish + fish !! 7
+        setAtIndex n = fish !! (n + 1)
+
+{-
+>>> growFish' [0,1,1,2,1,0,0,0,0]
+[1,1,2,1,0,0,0,0,0]
+
+>>> iterate growFish' [0,1,1,2,1,0,0,0,0] !! 80
+[424,729,558,790,739,762,991,370,571]
+
+-}
--- day06/test/Spec.hs
+++ day06/test/Spec.hs
@@ -2,18 +2,25 @@ import Control.Exception (evaluate)
 import Test.Hspec
 import Test.QuickCheck
 
-import Lib ()
+import Lib ( initialFish ,growFish, parse )
 
 main :: IO ()
 main = do
   testinput <- readFile "test/input-test.txt"
   hspec $ do
-    describe "function" $ do
-      it "should" $ do
-        True `shouldBe` True
+    describe "Testdata" $ do
+      it "should parse" $ do
+        initialFish (parse testinput) `shouldBe` [0,1,1,2,1,0,0,0,0]
+      it "should fit day 18" $ do
+        sum (iterate growFish [0,1,1,2,1,0,0,0,0] !! 18) `shouldBe` 26
+      it "should fit day 80" $ do
+        sum (iterate growFish [0,1,1,2,1,0,0,0,0] !! 80) `shouldBe` 5934
     describe "QuickCheck" $ do
-      it "property" $ do
-        property prop_LengthInvariantToDirection
+      it "Population grows" $ do
+        property prop_population_increases
 
-prop_Trivial :: Int -> Int -> Bool
-prop_Trivial x y = x + y == y + x
+prop_population_increases :: Int -> Bool
+prop_population_increases n = population n >= population n-1
+  where population n
+          | n > 0 = sum $ iterate growFish [0,1,1,2,1,0,0,0,0] !! n
+          | otherwise = 0
