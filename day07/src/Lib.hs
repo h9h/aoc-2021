930e5895c2ccbf323b2e6eabc66b29e3ba2392e3
heinke <git@h9h.de>
2021-12-07 07:07:11 +0100

    Day 07

--- day07/src/Lib.hs
+++ day07/src/Lib.hs
@@ -1,21 +1,63 @@
+{-# LANGUAGE TypeApplications #-}
 module Lib where
+
+import Data.List.Split ( wordsBy )
+import Data.Bits (Bits(xor))
 
 type X = Int
 
 puzzle :: IO ()
 puzzle = do
-    input <- lines <$> readFile "src/input.txt"
+    input <- readFile "src/input.txt"
     let xxx = parse input
     print $ take 1 xxx
-    print $ "Part 1: " ++ "todo"
-    print $ "Part 2: " ++ "todo"
+    print $ "Part 1: " ++ show (solve1 xxx)
+    print $ "Part 2: " ++ show (solve2 xxx)
 
 --
 -- Parse Input
 --
 
-parseLine :: String -> X
-parseLine _ = 1
+--
+-- Parse Input
+--
+parse :: String -> [Int]
+parse line = map (read @Int) $ wordsBy ( == ',') line
 
-parse :: [String] -> [X]
-parse = map parseLine
+--
+-- Calculate fuel
+--
+dist :: Num a => a -> a -> a
+dist y x = abs $ y - x
+
+deltaY :: Int -> [Int] -> Int
+deltaY y = sum <$> map (dist y)
+
+mins :: [Int] -> Int
+mins = foldr min maxBound
+
+maxs :: [Int] -> Int
+maxs = foldr max minBound
+
+solve1 xs = mins $ flip deltaY xs <$> range'
+  where range' = [mins xs..maxs xs]
+
+-- Part 2
+
+dist2 :: (Eq p, Num p, Enum p) => p -> p -> p
+dist2 y x
+  | y == x = 0
+  | otherwise = sum [0..abs $ y - x]
+
+deltaY2 :: Int -> [Int] -> Int 
+deltaY2 y = sum <$> map (dist2 y)
+
+solve2 xs = mins $ flip deltaY2 xs <$> range'
+  where range' = [mins xs..maxs xs]
+
+{-
+>>> deltaY2 2 [16,1,2,0,4,2,7,1,2,14]
+206
+>>> solve1 [16,1,2,0,4,2,7,1,2,14]
+37
+-}
--- day07/test/Spec.hs
+++ day07/test/Spec.hs
@@ -2,18 +2,20 @@ import Control.Exception (evaluate)
 import Test.Hspec
 import Test.QuickCheck
 
-import Lib ()
+import Lib (parse, deltaY, deltaY2, solve1, solve2)
 
 main :: IO ()
 main = do
   testinput <- readFile "test/input-test.txt"
+  let xs = parse testinput
   hspec $ do
-    describe "function" $ do
-      it "should" $ do
-        True `shouldBe` True
-    describe "QuickCheck" $ do
-      it "property" $ do
-        property prop_LengthInvariantToDirection
-
-prop_Trivial :: Int -> Int -> Bool
-prop_Trivial x y = x + y == y + x
+    describe "Part 1" $ do
+      it "should match example 1" $ do
+        deltaY 2 xs `shouldBe` 37
+      it "should match example 2" $ do
+        deltaY 1 xs `shouldBe` 41
+    describe "Part 2" $ do
+      it "should match example 1" $ do
+        deltaY2 2 xs `shouldBe` 206
+      it "should match example 2" $ do
+        deltaY2 5 xs `shouldBe` 168
