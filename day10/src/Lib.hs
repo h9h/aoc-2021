2e3176c75b4924659e053d5f34c5ce45b68ce691
heinke <git@h9h.de>
2021-12-10 18:08:51 +0100

    Day 10

--- day10/src/Lib.hs
+++ day10/src/Lib.hs
@@ -1,21 +1,64 @@
 module Lib where
 
-type X = Int
+import Data.List ( sort, elemIndex )
+import Control.Applicative ( liftA )
+
+data CheckResult = Ok | Incomplete [Char] | Corrupted Char deriving (Show, Eq)
 
 puzzle :: IO ()
 puzzle = do
     input <- lines <$> readFile "src/input.txt"
-    let xxx = parse input
-    print $ take 1 xxx
-    print $ "Part 1: " ++ "todo"
-    print $ "Part 2: " ++ "todo"
+    print $ head input
+    print $ "Part 1: " ++ show (part1 input)
+    print $ "Part 2: " ++ show (part2 input)
 
---
--- Parse Input
---
+openBrackets = "([{<"
+closeBrackets = ")]}>"
 
-parseLine :: String -> X
-parseLine _ = 1
+{-
+Idea:
+- push char onto stack
+- if next closes top of stack pop stack and continue with stack
+    else push next onto stack
+- remaining rest is either incomplete or corrupt
+-}
+removePairs :: [Char] -> [Char]
+removePairs = reverse . foldl removePair []
+            where
+                removePair ('(':ls) ')' = ls
+                removePair ('{':ls) '}' = ls
+                removePair ('[':ls) ']' = ls
+                removePair ('<':ls) '>' = ls
+                removePair ls c = c:ls
 
-parse :: [String] -> [X]
-parse = map parseLine
+
+removeOpenBrackets :: [Char] -> [Char]
+removeOpenBrackets [] = []
+removeOpenBrackets (c:xs)
+    | c `elem` openBrackets = removeOpenBrackets xs
+    | otherwise = c:xs
+
+categorise :: String -> CheckResult
+categorise = f . removePairs
+    where f s
+            | s == "" = Ok
+            | all (`notElem` s) closeBrackets = Incomplete s
+            | otherwise = Corrupted $ head $ removeOpenBrackets s
+
+costFalse :: Char -> Int
+costFalse c = case elemIndex c closeBrackets of
+    Just i -> [3, 57, 1197, 25137] !! i
+    Nothing -> 0
+
+part1 :: [String] -> Int
+part1 input = sum $ [costFalse c | (Corrupted c) <- map categorise input]
+
+costCorrect :: Int -> [Char] -> Int
+costCorrect m [] = m
+costCorrect m (c:cs) = case elemIndex c openBrackets of
+    Just i -> costCorrect (m * 5 + (i + 1)) cs
+    Nothing -> m
+
+part2 :: [String] -> Int
+part2 input = let l = [reverse x | (Incomplete x) <- map categorise input]
+          in  sort (map (costCorrect 0) l) !! (length l `div` 2)
--- day10/test/Spec.hs
+++ day10/test/Spec.hs
@@ -2,18 +2,50 @@ import Control.Exception (evaluate)
 import Test.Hspec
 import Test.QuickCheck
 
-import Lib ()
+import Lib ( 
+        CheckResult(..), 
+        categorise, 
+        removePairs, 
+        removeOpenBrackets, 
+        part1,
+        costCorrect,
+        part2
+        )
 
 main :: IO ()
 main = do
-  testinput <- readFile "test/input-test.txt"
+  testinput <- lines <$> readFile "test/input-test.txt"
   hspec $ do
-    describe "function" $ do
-      it "should" $ do
-        True `shouldBe` True
-    describe "QuickCheck" $ do
-      it "property" $ do
-        property prop_LengthInvariantToDirection
-
-prop_Trivial :: Int -> Int -> Bool
-prop_Trivial x y = x + y == y + x
+    describe "part1" $ do
+      it "removePairs Blank" $ do
+        removePairs "" `shouldBe` ""
+      it "removePairs OK" $ do
+        removePairs "<[(<>{})][<><>][{()()}]>" `shouldBe` ""
+      it "removePairs Corrupted" $ do
+        removePairs "<[(<>{})][<(><)>][{()()}]>" `shouldBe` "<[<(><)>]>"
+      it "removePairs Incomplete" $ do
+        removePairs "[({(<(())[]>[[{[]{<()<>>" `shouldBe` "[({([[{{"
+      it "removeOpenBrackets" $ do
+        removeOpenBrackets "<[(<>{})][<(><)>][{()()}]>" `shouldBe` ">{})][<(><)>][{()()}]>"
+      it "check testinput" $ do
+        map categorise testinput `shouldBe` 
+          [
+            Incomplete "[({([[{{",
+            Incomplete "({[<{(",
+            Corrupted '}',
+            Incomplete "((((<{<{{",
+            Corrupted ')',
+            Corrupted ']',
+            Incomplete "<{[{[{{[[",
+            Corrupted ')',
+            Corrupted '>',
+            Incomplete "<{(["
+          ]
+      it "should fit testdata part 1" $ do
+        part1 testinput `shouldBe` 26397
+    describe "part2" $ do
+      it "cost to correct" $ do
+        costCorrect 0 "[({([[{{" `shouldBe` 182193
+      it "should fit testdata part 2" $ do
+        part2 testinput `shouldBe` 288957
+      
