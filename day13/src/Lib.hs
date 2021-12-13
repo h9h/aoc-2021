d974f3035c458caf5dc5e212c7cf57a40656cf16
heinke <git@h9h.de>
2021-12-13 19:15:19 +0100

    Day 13

--- day13/src/Lib.hs
+++ day13/src/Lib.hs
@@ -1,21 +1,51 @@
 module Lib where
 
-type X = Int
+import Data.List ( elem )
+import Data.List.Split ( wordsBy, splitOn )
+import Data.Set ( toList, fromList )
+
+type Dot = (Int, Int)
+type Instruction = (String, Int)
 
 puzzle :: IO ()
 puzzle = do
-    input <- lines <$> readFile "src/input.txt"
-    let xxx = parse input
-    print $ take 1 xxx
-    print $ "Part 1: " ++ "todo"
-    print $ "Part 2: " ++ "todo"
+    input <- readFile "src/input.txt"
+    let (dots, instructions) = parse input
+    print $ take 1 dots
+    print instructions
+    print $ "Part 1: " ++ show (length $ fold dots (head instructions))
+    print "Part 2: " 
+    mapM_ putStrLn $ showCode (foldl fold dots instructions)
 
 --
 -- Parse Input
 --
 
-parseLine :: String -> X
-parseLine _ = 1
+parse :: String -> ([Dot], [Instruction])
+parse input = (parseDots, parseInstructions)
+    where
+        [dots, instructions] = map lines $ splitOn "\n\n" input
+        parseDots = map ((\[x,y] -> (x,y)) . map read . splitOn ",") dots
+        parseInstructions = map ((\[direction, n] -> (direction, read n)) . splitOn "=" . last . words) instructions
 
-parse :: [String] -> [X]
-parse = map parseLine
+fold :: [Dot] -> Instruction -> [Dot]
+fold dots instr = toList $ fromList $ f instr dots
+    where f instr dots
+            | fst instr == "x" = map (\(x,y) -> let n = snd instr in (n - abs(x - n), y)) dots
+            | fst instr == "y" = map (\(x,y) -> let n = snd instr in (x, n - abs(y - n))) dots
+            | otherwise  = dots
+
+showCode :: [Dot] -> [String]
+showCode points = [ 
+                       [ if (x, y) `elem` points then '█' else ' ' | x <- [0 .. h] ] | y <- [0 .. w]
+                   ]
+    where (h, w) = (maximum $ map fst points, maximum $ map snd points)
+
+{-
+ ##  #    ###  #### #  # #### #  # #  #  
+#  # #    #  # #    # #  #    # #  #  #  
+#  # #    #  # ###  ##   ###  ##   #  #  
+#### #    ###  #    # #  #    # #  #  #  
+#  # #    # #  #    # #  #    # #  #  #  
+#  # #### #  # #### #  # #    #  #  ##   
+-}
