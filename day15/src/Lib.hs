f567aa979c1a8129257569c2915e9435335850e6
heinke <git@h9h.de>
2021-12-15 16:28:47 +0100

    Day 15

--- day15/src/Lib.hs
+++ day15/src/Lib.hs
@@ -1,21 +1,62 @@
 module Lib where
 
-type X = Int
+import qualified Data.Map.Strict as M
+import           Algorithm.Search  ( dijkstra, aStar )
+
+type Cost  = Int
+type State = (Int, Int)
+type Grid  = M.Map State Cost
+
+output :: Maybe (Int, [(Int, Int)]) -> String
+output x = case x of
+  Just (n, _) -> show n
+  Nothing     -> error "fail"
 
 puzzle :: IO ()
 puzzle = do
     input <- lines <$> readFile "src/input.txt"
-    let xxx = parse input
-    print $ take 1 xxx
-    print $ "Part 1: " ++ "todo"
-    print $ "Part 2: " ++ "todo"
+    let grid = makeGrid 100 1 input
+    print $ "Part 1           : " ++ output (solve grid)
+    let grid2 = makeGrid 100 5 input
+    print $ "Part 2 - dijkstra: " ++ output (solve grid2)
+    print $ "Part 2 - a*      : " ++ output (solve2 grid2)
 
---
--- Parse Input
---
 
-parseLine :: String -> X
-parseLine _ = 1
+makeGrid :: Int -> Int -> [String] -> Grid
+makeGrid step copies rows = M.fromList
+  [ ((r + i * step, c + j * step), iterate addWrap(read [n]) !! (i+j))
+  | i        <- [0 .. copies-1]
+  , j        <- [0 .. copies-1]
+  , (r, row) <- zip [0 ..] rows
+  , (c, n  ) <- zip [0 ..] row
+  ]
+  where addWrap n = if n > 8 then 1 else n + 1
 
-parse :: [String] -> [X]
-parse = map parseLine
+{-
+Tatsächlich kann es günstiger sein auch mal einen Schritt zurückzutreten...
+(für meine Daten ergeben sich ansonsten (falsche) Kosten von 2816 statt 2809 im Teil 2)
+-}
+neighbors :: Grid -> State -> [State]
+neighbors grid (x, y) = filter (`M.member` grid) [(x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1)]
+
+cost :: Grid -> State -> State -> Cost
+cost grid _ state = grid M.! state
+
+finished :: Grid -> State -> Bool
+finished grid state = state == (fst . M.findMax) grid
+
+solve :: Grid -> Maybe (Cost, [State])
+solve grid = dijkstra (neighbors grid) (cost grid) (finished grid) (0,0)
+
+{-
+Alternativ statt Dijkstra mit a*
+-}
+
+estimate :: Grid -> State -> Cost
+estimate grid state = dx + dy
+  where (h, w) = (fst . M.findMax) grid
+        (x, y) = state
+        (dx, dy) = (h-x, w-y)
+
+solve2 :: Grid -> Maybe (Cost, [State])
+solve2 grid = aStar (neighbors grid) (cost grid) (estimate grid) (finished grid) (0,0)
--- day15/test/Spec.hs
+++ day15/test/Spec.hs
@@ -2,18 +2,34 @@ import Control.Exception (evaluate)
 import Test.Hspec
 import Test.QuickCheck
 
-import Lib ()
+import Lib ( Grid, makeGrid, solve)
+import qualified Data.Map.Strict as M
 
 main :: IO ()
 main = do
   testinput <- readFile "test/input-test.txt"
   hspec $ do
-    describe "function" $ do
-      it "should" $ do
-        True `shouldBe` True
-    describe "QuickCheck" $ do
-      it "property" $ do
-        property prop_LengthInvariantToDirection
-
-prop_Trivial :: Int -> Int -> Bool
-prop_Trivial x y = x + y == y + x
+    let grid = makeGrid 10 1 $ lines testinput
+    let grid2 = makeGrid 10 5 $ lines testinput
+    let grid3 = makeGrid 100 5 $ lines testinput
+    describe "makeGrid" $ do
+      it "read correct small grid" $ do
+        map (\n -> grid M.! (n,n)) [0 .. 9]   `shouldBe` [1,3,3,4,4,2,2,6,2,1]
+      it "read correct big grid small indices" $ do
+        map (\n -> grid2 M.! (n,n)) [0 .. 9]   `shouldBe` [1,3,3,4,4,2,2,6,2,1]
+      it "read correct big grid big indices" $ do
+        map (\n -> grid2 M.! (n,n)) [40 .. 49]   `shouldBe` [9,2,2,3,3,1,1,5,1,9]
+      it "read correct big grid small indices other diagonal" $ do
+        map (\n -> grid2 M.! (9-n,n)) [0 .. 9]   `shouldBe` [2,2,2,9,1,1,1,3,7,2]
+      it "read correct big grid big indices other diagonal" $ do
+        map (\n -> grid2 M.! (89-n,n)) [40 .. 49]   `shouldBe` [1,1,1,8,9,9,9,2,6,1]
+    describe "solve" $ do
+      it "Part1 should be 40 for test" $ do
+        f (solve grid) `shouldBe` 40
+      it "Part2 should be 315 for test" $ do
+        f (solve grid2) `shouldBe` 315
+        
+f :: Maybe (Int, [(Int, Int)]) -> Int
+f x = case x of
+  Just (n, _) -> n
+  Nothing     -> error "fail"
